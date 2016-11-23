5a8884a3afc30e5b4827c09e5d64a652eed30cc5
Yulia Gaponenko <barthy@yandex.ru>
2016-11-23 15:21:54 +0100

    Director can accept bosh events via api call
    [#133560817](https://www.pivotaltracker.com/story/show/133560817)
    
    Signed-off-by: Konstantin Maksimov <kmaksimov@ru.ibm.com>

--- bosh-director/lib/bosh/director/api/controllers/events_controller.rb
+++ bosh-director/lib/bosh/director/api/controllers/events_controller.rb
@@ -56,6 +56,40 @@ module Bosh::Director
         json_encode(events)
       end
 
+      post '/', :consumes => [:json] do
+        Config.logger.info("yulia: get post")
+        @permission_authorizer.granted_or_raise(:director, :admin, token_scopes)
+        payload = json_decode(request.body.read)
+        if payload['action'].nil? || payload['object_type'].nil? || payload['object_name'].nil?
+          raise ValidationInvalidType, 'Action, object_type, object_name are required'
+        end
+
+        error   = payload['error'].nil? ? nil : payload['error']
+        context = payload['context'].nil? ? nil : payload['context']
+        if !context.nil? && !context.kind_of?(Hash)
+          raise ValidationInvalidType, 'Context must be a hash'
+        end
+        begin
+          timestamp = payload['timestamp'].nil? ? nil : timestamp_filter_value(payload['timestamp'])
+        rescue ArgumentError, "Invalid timestamp parameter: '#{payload['timestamp']}' "
+        end
+
+        @event_manager.create_event(
+          {
+            parent_id:   nil,
+            timestamp:   timestamp,
+            user:        current_user,
+            action:      payload['action'],
+            object_type: payload['object_type'],
+            object_name: payload['object_name'],
+            deployment:  payload['deployment'],
+            instance:    payload['instance'],
+            task:        nil,
+            error:       error,
+            context:     context
+          })
+      end
+
       private
 
       def timestamp_filter_value(value)
